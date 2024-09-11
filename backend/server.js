28bee4bc27a127a3cf3dc260771383b53948ccdd
malik-na <malik2015naeem@gmail.com>
2024-09-11 11:02:05 +0530

    cors middleware added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import projectRoutes from "./routes/project.route.js";
 import teamRoutes from "./routes/team.routes.js";
+import cors from "cors";
 
 dotenv.config();
 const PORT = 5001;
@@ -10,6 +11,12 @@ const PORT = 5001;
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // This should be the URL of your frontend
+  })
+);
 
 app.use("/api/project", projectRoutes);
 app.use("/api/project", teamRoutes);
