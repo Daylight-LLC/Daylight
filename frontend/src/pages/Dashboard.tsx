87430e09153f0b1b0030017a6986fa9c544a4886
musaid1234 <m.41usaid@gmail.com>
2024-09-12 03:29:44 +0530

    dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import StatsCard from '../components/StatsCard';
 import MeetingCard from '../components/MeetingCard';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+
 
 
 
 const Dashboard: React.FC = () => {
 
-  async function getProjects() {
-    try{
-      const response = await fetch('http://localhost:5001/api/project')
-      console.log(response.json()) 
-    } catch{
-      console.log('error')
-    }
-  }
+  // async function getProjects() {
+  //   try{
+  //     const response = await fetch('http://localhost:5001/api/project')
+  //     console.log(response.json()) 
+  //   } catch{
+  //     console.log('error')
+  //   }
+  // }
 
-  getProjects()
+  // getProjects()
   return (
-    <div className="p-2 ">
-        <div className="flex">
-          <div className="w-3/4"> 
-            <h2 className="text-2xl font-semibold">Your Dashboard</h2>
-            <StatsCard/>
-          </div>
-
-          <div className="w-1/4 p-4 ">
-            <MeetingCard/>
+    <div className="p-2">
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-3/4 flex flex-col mb-4 md:mb-0">
+        <div className='w-full flex justify-between items-center'>
+          <h2 className="lg:text-2xl md:text-lg font-semibold">Your Dashboard</h2>
+          <div className='px-3 py-1 border border-gray-300 rounded-md flex items-center'>
+            last 30 days <ArrowDropDownOutlinedIcon className="ml-1" />
           </div>
         </div>
+        <StatsCard />
+      </div>
+
+      <div className="md:w-full lg:w-1/4 md:px-4 md:pb-4">
+        <MeetingCard />
+      </div>
     </div>
+  </div>
   );
 };
 
